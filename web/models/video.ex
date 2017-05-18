74fcc843e4bbfc85e9e4acda758899bab5362286
Ernesto Cojulun <erniecojulun@gmail.com>
2017-05-17 17:50:41 -0700

    adding categories

--- web/models/video.ex
+++ web/models/video.ex
@@ -6,6 +6,7 @@ defmodule Rumbl.Video do
     field :title, :string
     field :description, :string
     belongs_to :user, Rumbl.User
+    belongs_to :category, Rumbl.Category
 
     timestamps()
   end
@@ -14,7 +15,7 @@ defmodule Rumbl.Video do
   Builds a changeset based on the `struct` and `params`.
   """
   @required_fields ~w(url title description)
-  @optional_fields ~w()
+  @optional_fields ~w(category_id)
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, @required_fields, @optional_fields)

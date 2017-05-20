003a5e86436e03a7b468b16eddd3406b8db8d987
Ernesto Cojulun <erniecojulun@gmail.com>
2017-05-20 15:34:20 -0700

    Ch: 10 using channels
    connect to server through Es6
    built server side channel
    authenticate users via token when it comes to accessing channels
    persisting annotations

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -12,9 +12,9 @@ defmodule Rumbl.Auth do
     user_id = get_session(conn, :user_id)
     cond do
       user = conn.assigns[:current_user] ->
-        conn
-      user =  user_id && repo.get(Rumbl.User, user_id) ->
-        assign(conn, :current_user, user)
+        puts_current_user(conn, user)
+      user = user_id && repo.get(Rumbl.User, user_id) ->
+        puts_current_user(conn, user)
       true ->
         assign(conn, :current_user, nil)
     end
@@ -22,9 +22,16 @@ defmodule Rumbl.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> puts_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(render: true)
+  end
+
+  defp puts_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 
   def login_by_username_and_pass(conn, username, given_pass, opts) do
